fb6fa13039c95433a84b3a30937c467366e23802
Efraa <efra23peralta@gmail.com>
2022-05-07 12:37:18 -0400

    chore: ignore some directories

--- scripts/helpers/get-compose-files.sh
+++ scripts/helpers/get-compose-files.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 function get_compose_files() {
-    COMPOSE_FILES="-f ./docker/dc.networking.yml -f ./docker/dc.infrastructure.yml"
+    COMPOSE_FILES="-f ./docker/networking.yml -f ./docker/infrastructure.yml"
+
+    IGNORE_DIRS=(
+        "README.md"
+        "shared"
+        "rpc"
+        "realtime"
+        "http"
+        "postgres"
+        ".github"
+        ".github-private"
+    )
 
     [[ -z "$APPS_DIRECTORY" ]] && error "Oops! Environment variable APPS_DIRECTORY is not set."
 
@@ -12,12 +23,14 @@ function get_compose_files() {
 
     for APP_DIR in $APPS_DIRS; do
         APP_NAME="$(basename "$APP_DIR")"
-        COMPOSE_FILE="./docker/dc.$APP_NAME.yml"
+        COMPOSE_FILE="./docker/$APP_NAME.yml"
 
         if [[ -f "$COMPOSE_FILE" ]]; then
             COMPOSE_FILES="$COMPOSE_FILES -f $COMPOSE_FILE"
         else
-            [[ "$APP_NAME" == "README.md" ]] && continue
+            if [[ "${IGNORE_DIRS[*]}" == *"$APP_NAME"* ]] &>/dev/null; then
+                continue
+            fi
 
             warning "Oops! No compose file found for $APP_NAME. Skiping..."
         fi
