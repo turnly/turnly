596a23416b09ae54ce46459d2c76e44e3c900bbe
Efraa <efra23peralta@gmail.com>
2022-05-17 17:22:45 -0400

    build: remove postgres and elasticmq

--- scripts/helpers/print-hosts.sh
+++ scripts/helpers/print-hosts.sh
@@ -25,11 +25,8 @@ function print_hosts() {
   info " 🟢 Traffic Dashboard: http://$APP_PROXY_HOST:$APP_PROXY_DASHBOARD_PORT"
   info " 🟢 Elastic Search Dashboard: http://$APP_PROXY_HOST:$ELASTICSEARCH_KIBANA_PORT"
   info " 🟢 Mongo Dashboard: http://$APP_PROXY_HOST:$MONGO_UI_PORT"
-  info " 🟢 Postgres Dashboard: http://$APP_PROXY_HOST:$ADMINER_PORT"
   info " 🟢 Redis Dashboard: http://$APP_PROXY_HOST:$REDIS_ADMINER_PORT"
   info " 🟢 Storage Dashboard: http://$APP_PROXY_HOST:$MINIO_UI_PORT"
-  info " 🟢 ElasticMQ Dashboard: http://$APP_PROXY_HOST:$SQS_UI_PORT"
-  info " 🟢 RabbitMQ Dashboard: http://$APP_PROXY_HOST:$RABBITMQ_UI_PORT"
 
   line
 }
