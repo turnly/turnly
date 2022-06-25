78087285a13bbef83aa141d0e7700928aad8f9d6
Efra Peralta <efra23peralta@gmail.com>
2022-06-25 01:33:28 -0400

    fix: solve minor errors

--- apps/widgets-api/src/shared/GraphException.ts
+++ apps/widgets-api/src/shared/GraphException.ts
@@ -1,3 +1,4 @@
+import { ResponseCodes } from '@turnly/common'
 import { Meta } from '@turnly/rpc/dist/producers/addons'
 import { SharedMessages } from '@turnly/shared'
 import { ApolloError } from 'apollo-server-express'
@@ -6,8 +7,24 @@ export class GraphException extends ApolloError {
   constructor(meta?: Meta.AsObject) {
     const message = meta?.message ?? SharedMessages.UNKNOWN_EXCEPTION
 
-    super(message, String(meta?.status), meta)
+    super(
+      message,
+      GraphException.toGraphCode(meta?.status ?? ResponseCodes.INTERNAL_ERROR),
+      meta
+    )
 
     Object.defineProperty(this, 'name', { value: 'GraphException' })
+  }
+
+  private static toGraphCode(code: ResponseCodes): string {
+    const codes = {
+      [ResponseCodes.NOT_FOUND]: 'NOT_FOUND',
+      [ResponseCodes.BAD_REQUEST]: 'BAD_USER_INPUT',
+      [ResponseCodes.UNAUTHORIZED]: 'UNAUTHENTICATED',
+      [ResponseCodes.FORBIDDEN]: 'FORBIDDEN',
+      [ResponseCodes.CONFLICT]: 'BAD_USER_INPUT',
+    }
+
+    return codes[code] ?? 'INTERNAL_SERVER_ERROR'
   }
 }
