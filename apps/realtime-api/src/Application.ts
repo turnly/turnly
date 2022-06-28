d8bcca673b40a2b8d002dc36cb37bcaa7d64e729
Efra Peralta <efra23peralta@gmail.com>
2022-06-27 22:55:54 -0400

    build: update realtime package

--- apps/realtime-api/src/Application.ts
+++ apps/realtime-api/src/Application.ts
@@ -1,12 +1,12 @@
 import { Realtime } from '@turnly/realtime'
 import { eventBus } from '@turnly/shared'
+import { BroadcastingEventsSubscriber } from 'broadcasting/BroadcastingEventsSubscriber'
 import { helpdeskHandlers } from 'channels/helpdesk/handlers'
 import { queuingHandlers } from 'channels/queuing/handlers'
 import { AllowConnGuard } from 'channels/queuing/middlewares/AllowConnGuard'
 import { streamHandlers } from 'channels/stream/handlers'
 import { AuthorizedConnGuard } from 'channels/stream/middlewares/AuthorizedConnGuard'
 import { Channels, serverOptions } from 'shared/config'
-import { BroadcastingEventsSubscriber } from 'subscribers/BroadcastingEventsSubscriber'
 
 export class Application {
   private readonly realtime: Realtime
@@ -25,25 +25,30 @@ export class Application {
     this.setupBroadcastingEvents()
   }
 
+  /**
+   * Sets up channels.
+   *
+   * @description Create channels and register handlers.
+   */
   private setupChannels() {
-    /**
-     * Create a channel to listen to events
-     */
-    const stream = this.realtime.listen(Channels.STREAM)
-    const queuing = this.realtime.listen(Channels.QUEUING)
-    const helpdesk = this.realtime.listen(Channels.HELPDESK)
+    this.realtime
+      .listen(Channels.STREAM)
+      .use(new AuthorizedConnGuard().use())
+      .subscribe(streamHandlers)
 
-    /**
-     * Sets up the middleware
-     */
-    stream.use(new AuthorizedConnGuard().use())
-    queuing.use(new AllowConnGuard().use())
+    this.realtime
+      .listen(Channels.QUEUING)
+      .use(new AllowConnGuard().use())
+      .subscribe(queuingHandlers)
 
-    stream.subscribe(streamHandlers)
-    queuing.subscribe(queuingHandlers)
-    helpdesk.subscribe(helpdeskHandlers)
+    this.realtime.listen(Channels.HELPDESK).subscribe(helpdeskHandlers)
   }
 
+  /**
+   * Sets up broadcasting events.
+   *
+   * @description Register broadcasting subscriber to event bus.
+   */
   private setupBroadcastingEvents() {
     eventBus.subscribe([new BroadcastingEventsSubscriber()])
   }
