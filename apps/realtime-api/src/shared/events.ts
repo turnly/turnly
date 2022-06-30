4176aacad57e6703e1c5bfe86c6b0d2b254303dd
Efra Peralta <efra23peralta@gmail.com>
2022-06-30 09:17:30 -0400

    chore: add event names

---
+++ apps/realtime-api/src/shared/events.ts
@@ -0,0 +1,32 @@
+export enum Events {
+  /**
+   * Ticket events
+   */
+  TICKET_CREATED = 'ticket.created',
+  TICKET_AVAILABLE = 'ticket.available',
+  TICKET_ANNOUNCED = 'ticket.announced',
+  TICKET_COMPLETED = 'ticket.completed',
+  TICKET_CANCELLED = 'ticket.cancelled',
+  TICKET_DISCARDED = 'ticket.discarded',
+  TICKET_CALLED = 'ticket.called',
+  TICKET_RECALLED = 'ticket.recalled',
+  TICKET_NEAR_ATTENTION = 'ticket.near_attention',
+  TICKET_REMOVED = 'ticket.removed',
+  TICKET_INACTIVE = 'ticket.inactive',
+  TICKET_ASSIGNED = 'ticket.assigned',
+  TICKET_UNASSIGNED = 'ticket.unassigned',
+  TICKET_REASSIGNED = 'ticket.reassigned',
+
+  /**
+   * Customer events
+   */
+  CUSTOMER_CREATED = 'customer.created',
+  CUSTOMER_UPDATED = 'customer.updated',
+  CUSTOMER_DELETED = 'customer.deleted',
+
+  /**
+   * Global events
+   */
+  CONNECTED = 'connected',
+  DISCONNECTED = 'disconnected',
+}
