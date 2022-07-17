d233d22a452a36e6d708721c5406ad5652a49a24
Efra Peralta <efra23peralta@gmail.com>
2022-07-17 01:26:50 -0400

    ci: print variables

--- .github/actions/setup-mongodb/script.sh
+++ .github/actions/setup-mongodb/script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Setting up MongoDB with the following parameters:"
+echo ""
+echo "  - Username: $MONGO_USER"
+echo "  - Password: $MONGO_PASSWORD"
+echo "  - Database: $MONGO_DATABASE"
+
 docker exec mongo mongo --username "$MONGO_USERNAME" \
   --password "$MONGO_PASSWORD" --authenticationDatabase admin \
   --eval "db = db.getSiblingDB('$MONGO_DB'); \
