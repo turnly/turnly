753f50ca7e32c4cf5c5336ced1eb388128da162d
Efra Peralta <efra23peralta@gmail.com>
2022-07-17 01:05:17 -0400

    ci: improve github actions

---
+++ .github/actions/setup-mongodb/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker exec mongo mongo --username "$MONGO_USERNAME" \
+  --password "$MONGO_PASSWORD" --authenticationDatabase admin \
+  --eval "db = db.getSiblingDB('$MONGO_DB'); \
+  db.createUser({ user: '$MONGO_USERNAME', pwd: '$MONGO_PASSWORD', roles: [{ role: 'readWrite', db: '$MONGO_DB' }] });"
