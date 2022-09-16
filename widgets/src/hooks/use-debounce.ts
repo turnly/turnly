86999e0fc3e3d616665e4a267e950fe18f359f4d
Efra Peralta <efra23peralta@gmail.com>
2022-09-16 10:30:19 -0400

    chore(widgets): add use-debounce hook

---
+++ widgets/src/hooks/use-debounce.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'preact/hooks'
+
+export const useDebounce = <T>(value: T, delay: number): T => {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    const handler = setTimeout(() => setDebouncedValue(value), delay)
+
+    return () => {
+      clearTimeout(handler)
+    }
+  }, [value, delay])
+
+  return debouncedValue
+}
