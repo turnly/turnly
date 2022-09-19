2340eb2e3b2675904a48524c60acafcf7d2dab3d
Efra Peralta <efra23peralta@gmail.com>
2022-09-19 12:29:14 -0400

    chore(widgets): improve styles

--- widgets/src/components/app-portal.tsx
+++ widgets/src/components/app-portal.tsx
@@ -1,16 +1,19 @@
 import clsx from 'clsx'
 import { h } from 'preact'
 
+import { useAppearance } from '../hooks/use-appearance'
 import { useShowWidget } from '../hooks/use-show-widget'
 import styles from '../styles/portals/widget.module.scss'
 import { Portal } from './portal'
 
 export const AppPortal = props => {
   const { isShowing } = useShowWidget()
+  const { isFlat } = useAppearance()
 
   const classes = clsx({
     [styles['tly-widget']]: true,
     [styles['tly-widget--is-open']]: isShowing,
+    [styles['tly-widget--is-flat']]: isFlat,
   })
 
   return (
