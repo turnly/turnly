7f08b3b7780f6f2e8d9cc6f4211fb2c333b213bd
Efra Peralta <efra23peralta@gmail.com>
2022-09-22 22:01:26 -0400

    chore: remove react-query

--- widgets/src/app-container.tsx
+++ widgets/src/app-container.tsx
@@ -1,11 +1,9 @@
 import { ApolloProvider } from '@apollo/client'
-import { QueryClientProvider } from '@tanstack/react-query'
 import { h } from 'preact'
 
 import { AppShell } from './components/layouts/app-shell'
 import { useApolloClient } from './hooks/use-apollo-client'
 import { useTelemetry } from './hooks/use-telemetry'
-import { queryClient } from './libs/query-client'
 import { Router } from './routes'
 
 export const AppContainer = () => {
@@ -14,13 +12,11 @@ export const AppContainer = () => {
   useTelemetry()
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <ApolloProvider client={apolloClient}>
-        <AppShell>
-          <Router />
-        </AppShell>
-      </ApolloProvider>
-    </QueryClientProvider>
+    <ApolloProvider client={apolloClient}>
+      <AppShell>
+        <Router />
+      </AppShell>
+    </ApolloProvider>
   )
 }
 
