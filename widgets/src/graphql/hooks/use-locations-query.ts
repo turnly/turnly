ee0dce47e89138a541c4919663776598ea1afa66
Efra Peralta <efra23peralta@gmail.com>
2022-10-01 22:57:14 -0400

    chore(widgets): handle not found error in search results

--- widgets/src/graphql/hooks/use-locations-query.ts
+++ widgets/src/graphql/hooks/use-locations-query.ts
@@ -2,6 +2,7 @@ import * as Apollo from '@apollo/client'
 import { useMemo } from 'preact/hooks'
 
 import { Notifier } from '../../components/notification'
+import { useTranslation } from '../../localization'
 import {
   LocationsQuery as Query,
   LocationsQueryVariables as Variables,
@@ -11,6 +12,7 @@ import {
 export const useLocationsQuery = (
   options?: Apollo.QueryHookOptions<Query, Variables>
 ) => {
+  const { translate } = useTranslation()
   const {
     data,
     error,
@@ -19,7 +21,16 @@ export const useLocationsQuery = (
   } = useQuery({
     ...options,
     notifyOnNetworkStatusChange: true,
-    onError: error => Notifier.error(error.message),
+    onError: error => {
+      const isNotFound = error.graphQLErrors.some(
+        ({ extensions }) => extensions['code'] === 'NOT_FOUND'
+      )
+
+      if (isNotFound)
+        return Notifier.warning(translate('locations.labels.search_no_results'))
+
+      Notifier.error(error.message)
+    },
   })
 
   const nearby = useMemo(() => (data?.findLocations ?? []).slice(0, 3), [data])
