ca120f4760ab87ab8e459e440d6c495b8bf96a42
Jose Segura (josesp) <j0s3s3gur4@gmail.com>
2022-10-04 20:38:30 -0300

    feat: integrated internat-state to storage service, custom-fields, etc

---
+++ widgets/src/hooks/use-internal-state.ts
@@ -0,0 +1,37 @@
+import { useCallback } from 'preact/hooks'
+import create from 'zustand'
+import { persist } from 'zustand/middleware'
+import { immer } from 'zustand/middleware/immer'
+import shallow from 'zustand/shallow'
+
+import { ServiceParams } from '../components/services'
+import { Cookies } from '../libs/cookies'
+
+interface InternalStore {
+  selectedService?: ServiceParams
+  setService: (service?: ServiceParams) => void
+}
+
+const useStore = create<InternalStore>()(
+  persist(
+    immer(set => ({
+      service: undefined,
+      setService(service?: ServiceParams) {
+        set(state => {
+          state['selectedService'] = service
+        })
+      },
+    })),
+    {
+      name: 'tly.services.current',
+      version: 1,
+      getStorage: () => Cookies,
+    }
+  )
+)
+
+export const useInternalState = () =>
+  useStore(
+    useCallback(s => s, []),
+    shallow
+  )
